f23534ea078281007ea2aedde3fc4ce544c82d9b
Dmitry Osin <dmitry@osin.pw>
2017-05-28 18:54:48 +0300

    Added ShortUrlRepository

---
+++ Shortener.Web/Repository/ShortUrlRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Shortener.Web.Infrastructure;
+using Shortener.Web.Models;
+
+namespace Shortener.Web.Repository
+{
+    public class ShortUrlRepository : GenericRepository<ShortUrl>, IDisposable
+    {
+        public ShortUrlRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
