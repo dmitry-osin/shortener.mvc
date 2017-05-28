5e40a5bac6fd73e34575fde135b666b0ba1f7673
Dmitry Osin <dmitry@osin.pw>
2017-05-28 18:43:56 +0300

    Added EF

---
+++ Shortener.Web/Models/ShortUrl.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.Web.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string DateTime { get; set; }
+        public string Description { get; set; }
+    }
+}
