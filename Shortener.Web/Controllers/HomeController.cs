5fdd9c5361b57b136c8316c13b5a279becf14caf
Dmitry Osin <dmitry@osin.pw>
2017-05-29 03:00:40 +0300

    Fix

--- Shortener.Web/Controllers/HomeController.cs
+++ Shortener.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Shortener.Web.Infrastructure;
+using Shortener.Web.Models;
 using Shortener.Web.Repository;
 
 namespace Shortener.Web.Controllers
@@ -18,6 +19,25 @@ namespace Shortener.Web.Controllers
                 var list = ctx.GetAll();
             }
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new ShortUrl());
+        }
+
+        [HttpPost]
+        public ActionResult Create(ShortUrl url)
+        {
+            if (url == null) throw new ArgumentNullException(nameof(url));
+
+            using (var repo = new UrlRepository(AppDbContext.Create()))
+            {
+                repo.Add(url);
+            }
+
+            return RedirectToAction("");
         }
     }
 }
