fa68dc373529809864e582c032ebe2887c9eaa8f
Dmitry Osin <dmitry@osin.pw>
2017-06-11 19:40:16 +0300

    Added snackbar

---
+++ Shortener.Web/Scripts/snackBar.js
@@ -0,0 +1,37 @@
+﻿//$(document).ready(show);
+
+function show() {
+
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function copyToClipboard(elementId) {
+
+    // Create a "hidden" input
+    var aux = document.createElement("input");
+
+    // Assign it the value of the specified element
+    aux.setAttribute("value", document.getElementById(elementId).value);
+
+    // Append it to the body
+    document.body.appendChild(aux);
+
+    // Highlight its content
+    aux.select();
+
+    // Copy the highlighted text
+    document.execCommand("copy");
+
+    // Remove it from the body
+    document.body.removeChild(aux);
+
+    show();
+
+}
